90d7f409144ac6a6a867be179fc6dc215f9fe896
jmaldo28 <3167395jmaldo28@users.noreply.github.com>
2017-12-07 21:56:08 -0600

    button listener update

--- ButtonListener.java
+++ ButtonListener.java
@@ -1,23 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 
 public class ButtonListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if(!isValidMove()) {
-			return;
-		}
 		JButton button = (JButton) arg0.getSource();
-		button.setText(GameRunner.game.getCurrentPlayer().name);
+		GameRunner.pressed(button);
 		
 	}
-	
-	public boolean isValidMove() {
-		return true;
-	}
 }
-
-// test
